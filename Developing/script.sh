e98bf1c38994a3c5769dcb12bf6344bfc7fa5925
leonard <leonardmelnik@gmail.com>
2018-07-30 15:31:03 -0400

    Done with scripts for encrypting and decrypting, but not for cloning. To decrypt we copy the files to local storage, format the drive, then copy the files back

--- Developing/script.sh
+++ Developing/script.sh
@@ -11,3 +11,19 @@ cryptsetup --verbose -s 512 luksFormat /dev/sdc
 #How to decrypt
 #We must completly remove the encryption since we need to access it on other devices
 How about we copy the encrypted data to local storage, then immidiatly format the USB and copy the data over
+#open the drive
+sudo cryptsetup luksOpen /dev/sdb1 volume01
+#link it to a folder
+sudo mount /dev/mapper/volume01 /mnt/drive01
+#copy all the files to local storage
+cp -a /mtn/main_drive/. /dest/
+#format the flashdrive
+mkfs.vfat -n 'decrypted' -I /dev/sdb1
+#move the files back to the flashdrive
+cp -a /dest/. /media/root/decrypted
+#delete all of the folders that were created
+rm -R /mnt/main_drive 
+rm -R /dev/mapper/main_drive
+
+##Use https://www.loganmarchione.com/2015/05/encrypted-external-drive-with-luks/#Step_5_8211_Unlock_the_LUKS_device
+#To find more data
