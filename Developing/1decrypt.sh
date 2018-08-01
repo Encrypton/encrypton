281e9e904fb829e0ce5ed5441c6a283d37f72792
Leonard Melnik <leonardmelnik@gmail.com>
2018-08-01 11:57:29 -0400

    Works, error mounting: mount: wrong fs type, bad option, bad superblock on /dev/mapper/decrypt_drive,        missing codepage or helper program, or other error
           In some cases useful info is found in syslog - try
           dmesg | tail or so.

--- Developing/1decrypt.sh
+++ Developing/1decrypt.sh
@@ -4,19 +4,19 @@ echo "This works"
 #We must completly remove the encryption since we need to access it on other de$
 #How about we copy the encrypted data to local storage, then immidiatly format t$
 #open the drive
-sudo cryptsetup luksOpen /dev/sdb1 decrypt_drive
+sudo cryptsetup luksOpen /dev/sda decrypt_drive
 #link it to a folder
 mkdir -p /mnt/decrypt_drive
 mount /dev/mapper/decrypt_drive /mnt/decrypt_drive
 #copy all the files to local storage
-mv -v /mtn/decrypt_drive/. /home/pi/decrypt_drive/
+mv -v /mnt/decrypt_drive/. /home/pi/decrypt_drive/
 #format the flashdrive
 umount /mnt/decrypt_drive/
 cryptsetup luksClose /dev/mapper/decrypt_drive
-mkfs.vfat -n 'decrypted' -I /dev/sda1
+mkfs.vfat -n 'decrypted' -I /dev/sdc1
 #move the files back to the flashdrive
 mkdir -p /mnt/decrypted
-mount /dev/sdb1 /mnt/decrypted
+mount /dev/sdc1 /mnt/decrypted
 mv -v /home/pi/decrypt_drive/. /mnt/decrypted/
 #delete all of the folders that were created
 umount /mnt/decrypted
