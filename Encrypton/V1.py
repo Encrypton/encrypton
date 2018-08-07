922172c2e9d6b6ea39236b50a33d37cfbe12031a
JYDM <johnwallar@gmail.com>
2018-08-07 10:15:47 -0400

    Logos and conflict reso

--- Encrypton/V1.py
+++ Encrypton/V1.py
@@ -14,6 +14,7 @@ import RPi.GPIO as GPIO
 import os
 import shutil
 import zipfile
+import subprocess
 
 # Raspberry Pi pin setup
 lcd_rs = 25
@@ -30,57 +31,6 @@ lcd_columns = 16      # Define LCD column and row size for 16x2 LCD.
 lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-def encryptfile(key, filename):
-	chunksize = 64 * 1024
-	outFile = os.path.join(os.path.dirname(filename), "(encrypted)" + os.path.basename(filename))
-	filesize = str(os.path.getsize(filename)).zfill(16)
-	IV = ''
-
-	for i in range(16):
-		IV += chr(random.randint(0, 0xFF))
-
-	encryptor = AES.new(key, AES.MODE_CBC, IV)
-
-	with open(filename, "rb") as infile:
-		with open(outFile, "wb") as outfile:
-			outfile.write(filesize)
-			outfile.write(IV)
-			while True:
-				chunk = infile.read(chunksize)
-
-				if len(chunk) == 0:
-					break
-				elif len(chunk) % 16 != 0:
-					chunk += ' ' * (16 - (len(chunk) % 16))
-				outfile.write(encryptor.encrypt(chunk))
-
-
-def decryptfile(key, filename):
-	outFile = os.path.join(os.path.dirname(filename), os.path.basename(filename[11:]))
-	chunksize = 64 * 1024
-	with open(filename, "rb") as infile:
-		filesize = infile.read(16)
-		IV = infile.read(16)
-
-		decryptor = AES.new(key, AES.MODE_CBC, IV)
-
-		with open(outFile, "wb") as outfile:
-			while True:
-				chunk = infile.read(chunksize)
-				if len(chunk) == 0:
-					break
-
-				outfile.write(decryptor.decrypt(chunk))
-
-			outfile.truncate(int(filesize))
-
-
-def allfiles():
-	allFiles = []
-	#for root, subfiles, files in os.walk(os.getcwd()):
-		#for names in files:
-			#allFiles.append(os.path.join(root, names))
-	return allFiles
 
 
 def encrypt():
@@ -97,6 +47,9 @@ def encrypt():
 	lcd.message("Press ENCRYPT \nagain to confirm")                                          # # Confirm
 	while True:
 		if not GPIO.input(16): break
+    lcd.message("Please scan \n ID Card")
+    rfid = raw.input()
+    subprocess.call()
 	# encryptfile((raw_input, flashdir))                                                      # # ENCRYPTION
 
 	while periodcount <= 5 and periodover <= 1:                                              # # Loading...
---
+++ Encrypton/encyrptziptest.py
@@ -0,0 +1,3 @@
+Import pyminzip
+level=4 #level of compression
+pyminizip.compress("ENCRYPT/", "ENCRYPTED.zip", "password", level)
