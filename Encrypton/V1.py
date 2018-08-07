b944e970fe2e276001c4b8089fb3895e60b02645
JYDM <johnwallar@gmail.com>
2018-08-07 10:55:57 -0400

    fixes

--- Encrypton/V1.py
+++ Encrypton/V1.py
@@ -32,8 +32,6 @@ lcd_columns = 16      # Define LCD column and row size for 16x2 LCD.
 lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-<<<<<<< HEAD
-=======
 print("[ " + time.asctime() + " ]   LCD Setup Complete")
 
 GPIO.setmode(GPIO.BCM)
