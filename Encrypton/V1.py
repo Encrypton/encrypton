c31a816e3294c57e32e79835ca77ffc1c4e90554
leonard <leonardmelnik@gmail.com>
2018-08-07 10:59:23 -0400

    removed python encryption, moving to 7zip

--- Encrypton/V1.py
+++ Encrypton/V1.py
@@ -40,51 +40,6 @@ GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 print("[ " + time.asctime() + " ]   Button Setup Complete")
 
-def encryptfile(key, filename):
-	chunksize = 64 * 1024
-	outFile = os.path.join(os.path.dirname(filename), "(encrypted)" + os.path.basename(filename))
-	filesize = str(os.path.getsize(filename)).zfill(16)
-	IV = ''
-
-	for i in range(16):
-		IV += chr(random.randint(0, 0xFF))
-
-	encryptor = AES.new(key, AES.MODE_CBC, IV)
-
-	with open(filename, "rb") as infile:
-		with open(outFile, "wb") as outfile:
-			outfile.write(filesize)
-			outfile.write(IV)
-			while True:
-				chunk = infile.read(chunksize)
-
-				if len(chunk) == 0:
-					break
-				elif len(chunk) % 16 != 0:
-					chunk += ' ' * (16 - (len(chunk) % 16))
-				outfile.write(encryptor.encrypt(chunk))
-
-
-def decryptfile(key, filename):
-	outFile = os.path.join(os.path.dirname(filename), os.path.basename(filename[11:]))
-	chunksize = 64 * 1024
-	with open(filename, "rb") as infile:
-		filesize = infile.read(16)
-		IV = infile.read(16)
-
-		decryptor = AES.new(key, AES.MODE_CBC, IV)
-
-		with open(outFile, "wb") as outfile:
-			while True:
-				chunk = infile.read(chunksize)
-				if len(chunk) == 0:
-					break
-
-				outfile.write(decryptor.decrypt(chunk))
-
-			outfile.truncate(int(filesize))
-
-
 def allfiles():
 	allFiles = []
 	#for root, subfiles, files in os.walk(os.getcwd()):
