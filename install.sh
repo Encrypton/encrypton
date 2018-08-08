f602d106dcd1ea4c27c05ed0973600c28fd02b26
leonard <leonardmelnik@gmail.com>
2018-08-08 14:08:24 -0400

    Added install 7zip, added to make folder

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Thanks for installing Encrypton!!
 sudo apt-get update
-sudo apt-get install build-essential python-dev python-smbus python-pip
+sudo apt-get install build-essential python-dev python-smbus python-pip -y
 sudo pip install RPi.GPIO
 cd | git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git
 sudo python Adafruit_Python_CharLCD/setup.py install
-sudo apt-get install exfat-fuse exfat-utils
+sudo apt-get install exfat-fuse exfat-utils -y
+sudo apt-get install p7zip-full -y
+sudo mkdir /home/pi/Encrypton
