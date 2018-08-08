f577525983f2f1b3bdaae89809878e2f30d8991a
leonard <leonardmelnik@gmail.com>
2018-08-08 13:54:42 -0400

    finailizing install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #Thanks for installing Encrypton!!
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-smbus python-pip
-cd Dependencies | sudo pip install RPi.GPIO
+sudo pip install RPi.GPIO
+cd | git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git
 sudo python Adafruit_Python_CharLCD/setup.py install
 sudo apt-get install exfat-fuse exfat-utils
