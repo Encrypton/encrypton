9e47626939c2a4aceb31466858918c07a84ac237
Leonard Melnik <lmelnik00@stuy.edu>
2018-08-07 21:31:21 -0400

    Spaced

--- Bin/Versions/V4.py
+++ Bin/Versions/V4.py
@@ -165,9 +165,11 @@ def clone():
 	os.system("sudo cp -a /media/pi/MAIN /media/pi/CLONE")
 	os.system("sudo cp -a /media/pi/MAIN /media/pi/CLONE1")
 	os.system("sudo cp -a /media/pi/MAIN /media/pi/CLONE2")
+
 	os.system("sudo cp -a /media/pi/MAIN1 /media/pi/CLONE")
 	os.system("sudo cp -a /media/pi/MAIN1 /media/pi/CLONE1")
 	os.system("sudo cp -a /media/pi/MAIN1 /media/pi/CLONE2")
+    
 	os.system("sudo cp -a /media/pi/MAIN2 /media/pi/CLONE")
 	os.system("sudo cp -a /media/pi/MAIN2 /media/pi/CLONE1")
 	os.system("sudo cp -a /media/pi/MAIN2 /media/pi/CLONE2")
